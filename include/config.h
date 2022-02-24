8b9533ec6e458e351c2621bc24e36ca981eacda4
dr. josh butler <josh.butler929@gmail.com>
2022-02-23 21:15:37 -0500

    Add motor relay

--- include/config.h
+++ include/config.h
@@ -5,9 +5,10 @@
 #define RED_LED     5
 #define GREEN_LED   18
 #define MOTOR_SNSR  4
+#define MOTOR_RELAY 27
 #define BUTTON      19
 
-#define GPIO_OUTPUT_PIN_SEL     ((1ULL<<RED_LED) | (1ULL<<GREEN_LED) | (1ULL<<ONBOARD_LED))
+#define GPIO_OUTPUT_PIN_SEL     ((1ULL<<RED_LED) | (1ULL<<GREEN_LED) | (1ULL<<ONBOARD_LED) | (1ULL<<MOTOR_RELAY))
 #define GPIO_INPUT_PIN_SEL      ((1ULL<<MOTOR_SNSR))
 #define GPIO_BUTTON_SEL         ((1ULL<<BUTTON))
 #define ESP_INTR_FLAG_DEFAULT   0
