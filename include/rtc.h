109f9907ef6369da5f45eb4ab513d7ae6e96d44d
dr. josh butler <31372342+shuki25@users.noreply.github.com>
2022-07-11 20:45:58 -0500

    fix preprocessing bug

--- include/rtc.h
+++ include/rtc.h
@@ -3,10 +3,10 @@
 
 #if ONBOARD_RTC
 #include "pcf8563.h"
-#endif
 
 esp_err_t external_rtc_init(i2c_dev_t *dev);
 esp_err_t sync_rtc_clock(i2c_dev_t *dev, char *tz);
 esp_err_t set_rtc_time(i2c_dev_t *dev, struct tm *time);
 
+#endif
 #endif
